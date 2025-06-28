a4d045c7647a9a0872290abdfcf1dbd8e6668e82
nonti <nonty8@gmail.com>
2025-06-28 11:12:23 +0200

    modified contact section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom"
-import {Home} from "./pages/Home"
-import NotFound from "./pages/NotFound"
+import {Home} from "@/pages/Home"
+import NotFound from "@/pages/NotFound"
+import { Toaster } from "@/components/ui/toaster"
 
 const App = () => {
 
   return (
     <>
+    <Toaster/>
      <Routes>
       <Route index element={<Home/>}/>
       <Route path='*' element={<NotFound/>}/>
--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -1,58 +1,111 @@
-import { Mail, MapPin, Phone } from "lucide-react"
-
+import { Instagram, Linkedin, Mail, MapPin, Phone, Send, Twitter } from 'lucide-react'
+import { cn } from '@/lib/utils.js';
+import { useToast } from '../hooks/use-toast';
+import { useState } from 'react';
 export const ContactSection = () => {
+  const { toast } = useToast();
+  const [isSubmitting, setIsSubmitting] = useState(false)
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
+    setIsSubmitting(true);
+
+    setTimeout(() => {
+      toast({
+        title: 'Message Sent!',
+        description: 'Thank you for your message. I&apos;ll get back to you soon.'
+      });
+      setIsSubmitting(false);
+    }, 1500);
+  };
   return (
-    <section id='#contact' className="py-24 px-4 relative bg-secondary">
-      <div className="container mx-auto max-w-5xl">
+    <section id='#contact' className='py-24 px-4 relative bg-secondary'>
+      <div className='container mx-auto max-w-5xl'>
         <h2>
-        Get in <span className="text-primary">Touch</span>
+        Get in <span className='text-primary'>Touch</span>
       </h2>
 
-      <p className="text-center text-muted-foreground max-w-2xl mx-auto">
+      <p className='text-center text-muted-foreground max-w-2xl mx-auto'>
         Have a project in mind or collaborate?  Feel free to reach out. I'm always open to discussing new opportunities.
       </p>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
-        <div className="space-y-8">
-          <h3 className="text-2xl font-semibold mb-6">Contact Information</h3>
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-12'>
+        <div className='space-y-8'>
+          <h3 className='text-2xl font-semibold mb-6'>Contact Information</h3>
 
-          <div className="space-y-6 justify-center">
-            <div className="flex items-start space-x-4">
-              <div className="p-3 rounded-full bg-primary/10">
-                <Mail className="w-6 h-6 text-primary"/>
+          <div className='space-y-6 justify-center'>
+            <div className='flex items-start space-x-4'>
+              <div className='p-3 rounded-full bg-primary/10'>
+                <Mail className='w-6 h-6 text-primary'/>
               </div>
 
               <div>
                 <h4>Email</h4>
-                <a href="mailto:nonty8@gmail.com" className="text-muted-foreground hover:text-primary transition-colors">nonty8@gmail.com</a>
+                <a href='mailto:nonty8@gmail.com' className='text-muted-foreground hover:text-primary transition-colors'>nonty8@gmail.com</a>
               </div>
             </div>
           </div>
-          <div className="space-y-6 justify-center">
-            <div className="flex items-start space-x-4">
-              <div className="p-3 rounded-full bg-primary/10">
-                <Phone className="w-6 h-6 text-primary"/>
+          <div className='space-y-6 justify-center'>
+            <div className='flex items-start space-x-4'>
+              <div className='p-3 rounded-full bg-primary/10'>
+                <Phone className='w-6 h-6 text-primary'/>
               </div>
 
               <div>
                 <h4>Cell Phone</h4>
-                <a href="tel:+27797684297" className="text-muted-foreground hover:text-primary transition-colors">+27 (79) 768-4297</a>
+                <a href='tel:+27797684297' className='text-muted-foreground hover:text-primary transition-colors'>+27 (79) 768-4297</a>
               </div>
             </div>
           </div>
 
-          <div className="space-y-6 justify-center">
-            <div className="flex items-start space-x-4">
-              <div className="p-3 rounded-full bg-primary/10">
-                <MapPin className="w-6 h-6 text-primary"/>
+          <div className='space-y-6 justify-center'>
+            <div className='flex items-start space-x-4'>
+              <div className='p-3 rounded-full bg-primary/10'>
+                <MapPin className='w-6 h-6 text-primary'/>
               </div>
 
               <div>
                 <h4>Location</h4>
-                <a href="mailto:nonty8@gmail.com" className="text-muted-foreground hover:text-primary transition-colors">481 Cuba Crescent, Lulekani</a>
+                <a href='mailto:nonty8@gmail.com' className='text-muted-foreground hover:text-primary transition-colors'>481 Cuba Crescent, Lulekani</a>
               </div>
             </div>
           </div>
+          <div className='pt-8'>
+            <h4 className='font-medium mb-4'>Coneect with me</h4>
+            <div className='flex space-x-4 justify-center'>
+              <a href=''>
+                <Linkedin/>
+              </a>
+              <a href=''>
+                <Twitter/>
+              </a>
+              <a href=''>
+                <Instagram/>
+              </a>
+            </div>
+          </div>
+
+        </div>
+        <div className='bg-card p-8 rounded-lg shadow-xs'>
+            <h3 className='text-2xl font-semibold mb-6'>Send a Message</h3>
+            <form onSubmit={handleSubmit} className='space-y-6'>
+              <div>
+                <label htmlFor='Name' className='block text-sm font-medium mb-2'>Name</label>
+                <input type='text' placeholder='Jonh Doe' required id='name'  name='name' className='w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden foucs:ring-2 focus:ring-primary' />
+              </div>
+              <div>
+                <label htmlFor='email' className='block text-sm font-medium mb-2'>Email</label>
+                <input type='email' placeholder='name@mail.com' required id='email'  name='email' className='w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden foucs:ring-2 focus:ring-primary' />
+              </div>
+              <div>
+                <label htmlFor='message' className='block text-sm font-medium mb-2'>Message</label>
+                <textarea type='text' placeholder='Message' required id='message'  name='message' className='w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden foucs:ring-2 focus:ring-primary' />
+              </div>
+
+              <button className={cn('cosy-button w-full flex items-center justify-center gap-2')}>
+                {isSubmitting ? 'Sending...': 'Send Message'}
+                <Send size={16}/>
+              </button>
+            </form>
         </div>
       </div>
       </div>
